c63e3f76a454e145bf5c25b907a8724fd4c5448d
StefanBurscher <stefan.burscher@gmail.com>
2019-04-13 16:47:34 +0200

    ...

--- frontend/src/js/components/pages/LoginPage/LoginPage.js
+++ frontend/src/js/components/pages/LoginPage/LoginPage.js
@@ -16,6 +16,32 @@ class LoginPage extends React.Component {
     }
   }
 
+  renderLoginForm = () => {
+    return (
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <p className='text-uppercase'>Email address</p>
+          <Form.Control type='email' placeholder='Enter email' />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <p className='text-uppercase'>Password</p>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Button
+          variant='primary'
+          type='submit'
+          className='w-100 mt-2 defaultButton'
+        >
+          Sign in
+        </Button>
+        <p className='mt-3 text-center'>
+          Don't have an account? <Link to='/register'>Sign up</Link>
+        </p>
+      </Form>
+    )
+  }
+
   render () {
     return (
       <SplitLayout>
@@ -35,27 +61,7 @@ class LoginPage extends React.Component {
             </Link>
             <h3>Sign in</h3>
             <p>Please enter your credentials to proceed.</p> <br />
-            <Form>
-              <Form.Group controlId='formBasicEmail'>
-                <p className='text-uppercase'>Email address</p>
-                <Form.Control type='email' placeholder='Enter email' />
-              </Form.Group>
-
-              <Form.Group controlId='formBasicPassword'>
-                <p className='text-uppercase'>Password</p>
-                <Form.Control type='password' placeholder='Password' />
-              </Form.Group>
-              <Button
-                variant='primary'
-                type='submit'
-                className='w-100 mt-2 defaultButton'
-              >
-                Sign in
-              </Button>
-              <p className='mt-3 text-center'>
-                Don't have an account? <Link to='/register'>Sign up</Link>
-              </p>
-            </Form>
+            {this.renderLoginForm()}
           </Card>
         </div>
       </SplitLayout>
