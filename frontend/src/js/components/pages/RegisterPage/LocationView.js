6c8d7c6d97e3f054b44a5f863275f67250fed476
StefanBurscher <stefan.burscher@gmail.com>
2019-04-13 16:37:05 +0200

    ...

--- frontend/src/js/components/pages/RegisterPage/LocationView.js
+++ frontend/src/js/components/pages/RegisterPage/LocationView.js
@@ -5,27 +5,57 @@ import Autocomplete from 'react-google-autocomplete'
 import MyMap from '../../atoms/MyMap/MyMap'
 
 class LocationView extends React.Component {
+  renderAutoComplete = () => {
+    return (
+      <Autocomplete
+        className='form-control'
+        onPlaceSelected={place => {
+          const lat = place.geometry.location.lat()
+          const lng = place.geometry.location.lng()
+          this.props.setLocation({
+            lat,
+            lng,
+            formatted_address: place.formatted_address,
+            url: place.url
+          })
+        }}
+        types={[]}
+      />
+    )
+  }
+  renderButtons = () => {
+    return (
+      <Row className='mt-4'>
+        <Col>
+          <Button
+            variant='outline-primary'
+            type='submit'
+            onClick={this.props.goToPrevStep}
+            className='w-100 ghostButton defaultButton'
+          >
+            Previous
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            variant='primary'
+            type='submit'
+            onClick={this.props.goToNextStep}
+            className='w-100 border-0 defaultButton'
+          >
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    )
+  }
   render () {
-    console.log('this.props', this.props)
     const { location } = this.props.state
     return (
       <Form>
         <Form.Group controlId='location'>
           <Form.Label>Your solar panel location</Form.Label>
-          <Autocomplete
-            className='form-control'
-            onPlaceSelected={place => {
-              const lat = place.geometry.location.lat()
-              const lng = place.geometry.location.lng()
-              this.props.setLocation({
-                lat,
-                lng,
-                formatted_address: place.formatted_address,
-                url: place.url
-              })
-            }}
-            types={[]}
-          />
+          {this.renderAutoComplete()}
         </Form.Group>
         <MyMap
           defaultZoom={18}
@@ -33,28 +63,7 @@ class LocationView extends React.Component {
           lng={location.lng}
           isMarkerShown
         />
-        <Row className='mt-4'>
-          <Col>
-            <Button
-              variant='outline-primary'
-              type='submit'
-              onClick={this.props.goToPrevStep}
-              className='w-100 ghostButton defaultButton'
-            >
-              Previous
-            </Button>
-          </Col>
-          <Col>
-            <Button
-              variant='primary'
-              type='submit'
-              onClick={this.props.goToNextStep}
-              className='w-100 border-0 defaultButton'
-            >
-              Submit
-            </Button>
-          </Col>
-        </Row>
+        {this.renderButtons()}
       </Form>
     )
   }
